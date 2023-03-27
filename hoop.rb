6cebea6b1b302b20cb330134949f9a0fa5634d30
hoophq <team@runops.io>
2023-03-27 14:31:29 -0300

    Brew formula update for hoop version 1.4.4

--- hoop.rb
+++ hoop.rb
@@ -5,20 +5,20 @@
 class Hoop < Formula
   desc "Hoop allows acessing any infra-structure resource easily and secure."
   homepage "https://github.com/hoophq/hoopcli"
-  version "1.4.3"
+  version "1.4.4"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://hoopartifacts.s3.amazonaws.com/release/1.4.3/hoop_1.4.3_Darwin_x86_64.tar.gz"
-      sha256 "1332d9aed41f663140a1ee77ed5324cac9d1be92dc28115681a0f6781f3b29ce"
+    if Hardware::CPU.arm?
+      url "https://hoopartifacts.s3.amazonaws.com/release/1.4.4/hoop_1.4.4_Darwin_arm64.tar.gz"
+      sha256 "dcef22667118b1c4d46926cf6e2720dc0b4c6516d5a4be971e3890f4059a5dfc"
 
       def install
         bin.install "hoop"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://hoopartifacts.s3.amazonaws.com/release/1.4.3/hoop_1.4.3_Darwin_arm64.tar.gz"
-      sha256 "4c13fac2f6155115d71cbb9e0057760b46456ec143bc826ceb9ec017e0d2071c"
+    if Hardware::CPU.intel?
+      url "https://hoopartifacts.s3.amazonaws.com/release/1.4.4/hoop_1.4.4_Darwin_x86_64.tar.gz"
+      sha256 "5372f66626603a7a7ee0e37a372cb87090d89ef7f7d2f1a3f609c0b8b30bd1f7"
 
       def install
         bin.install "hoop"
@@ -27,17 +27,17 @@ class Hoop < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://hoopartifacts.s3.amazonaws.com/release/1.4.3/hoop_1.4.3_Linux_x86_64.tar.gz"
-      sha256 "2e440e8b38fcac84867bc1c8a7b99ab044cc2fcfaeb91abd44e17d8de6be6cf5"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://hoopartifacts.s3.amazonaws.com/release/1.4.4/hoop_1.4.4_Linux_arm64.tar.gz"
+      sha256 "df3d5ee6ec60a9e128032f2b197f0f4e43ab1fe885eb7d166a41a23ae2f76825"
 
       def install
         bin.install "hoop"
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://hoopartifacts.s3.amazonaws.com/release/1.4.3/hoop_1.4.3_Linux_arm64.tar.gz"
-      sha256 "1bca8b1d9a9a7e335eb64431114d4fab850f06c5038bde6dcfc1954c84a6a8fa"
+    if Hardware::CPU.intel?
+      url "https://hoopartifacts.s3.amazonaws.com/release/1.4.4/hoop_1.4.4_Linux_x86_64.tar.gz"
+      sha256 "ab49e13aa0d50dff933ce3994ff60e69145b1c7d480f618a156114d9bffb0314"
 
       def install
         bin.install "hoop"
